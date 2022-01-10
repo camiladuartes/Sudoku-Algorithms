004ab1b8c7bdc77e9903d24bb18dba2a3923b949
Camila Duarte <camiladuartes_@ufrn.edu.br>
2022-01-09 23:28:55 -0300

    adding some comments to the exact algorithm

--- exact-algorithm/include/backtracking.h
+++ exact-algorithm/include/backtracking.h
@@ -15,11 +15,13 @@ namespace bt{
 
         int line = floor(x / 9); 
         int count = 0;
+        // vertices of the same line
         while (count < 9){
             returnList.push_back(list[line * 9 + count]);
             count += 1;
         }
 
+        // vertices of the same column
         int column = x % 9;
         count = 0;
         while (count < 9){
@@ -27,6 +29,8 @@ namespace bt{
             count += 1;
         }
 
+        // vertices of the same block
+        // first line of each block, first column
         if ((x % 27 == 0) || (x % 27 == 3) || (x % 27 == 6)){
             returnList.push_back(list[x + 10]);
             returnList.push_back(list[x + 11]);
@@ -34,6 +38,7 @@ namespace bt{
             returnList.push_back(list[x + 20]);
         }
 
+        // first line of each block, second column
         if ((x % 27 == 1) || (x % 27 == 4) || (x % 27 == 7)){
             returnList.push_back(list[x + 8]);
             returnList.push_back(list[x + 10]);
@@ -41,6 +46,7 @@ namespace bt{
             returnList.push_back(list[x + 19]);
         }
 
+        // first line of each block, thrid column
         if ((x % 27 == 2) || (x % 27 == 5) || (x % 27 == 8)){
             returnList.push_back(list[x + 7]);
             returnList.push_back(list[x + 8]);
@@ -48,6 +54,7 @@ namespace bt{
             returnList.push_back(list[x + 17]);
         }
 
+        // second line of each block, first column
         if ((x % 27 == 9) || (x % 27 == 12) || (x % 27 == 15)){
             returnList.push_back(list[x - 8]);
             returnList.push_back(list[x - 7]);
@@ -55,6 +62,7 @@ namespace bt{
             returnList.push_back(list[x + 11]);
         }
 
+        // second line of each block, second column
         if ((x % 27 == 10) || (x % 27 == 13) || (x % 27 == 16)){
             returnList.push_back(list[x - 10]);
             returnList.push_back(list[x - 8]);
@@ -62,6 +70,7 @@ namespace bt{
             returnList.push_back(list[x + 10]);
         }
 
+        // second line of each block, thrid column
         if ((x % 27 == 11) || (x % 27 == 14) || (x % 27 == 17)){
             returnList.push_back(list[x - 11]);
             returnList.push_back(list[x - 10]);
@@ -69,13 +78,15 @@ namespace bt{
             returnList.push_back(list[x + 8]);
         }
 
+        // third line of each block, first column
         if ((x % 27 == 18) || (x % 27 == 21) || (x % 27 == 24)){
             returnList.push_back(list[x - 17]);
             returnList.push_back(list[x - 16]);
             returnList.push_back(list[x - 8]);
             returnList.push_back(list[x - 7]);
         }
-
+        
+        // third line of each block, second column
         if ((x % 27 == 19) || (x % 27 == 22) || (x % 27 == 25)){
             returnList.push_back(list[x - 19]);
             returnList.push_back(list[x - 17]);
@@ -83,6 +94,7 @@ namespace bt{
             returnList.push_back(list[x - 8]);
         }
 
+        // third line of each block, thrid column
         if ((x % 27 == 20) || (x % 27 == 23) || (x % 27 == 26)){
             returnList.push_back(list[x - 20]);
             returnList.push_back(list[x - 19]);
